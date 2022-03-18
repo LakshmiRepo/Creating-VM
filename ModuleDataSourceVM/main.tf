43b45a51657e0d1d9a27944f806ad0437125dced
LakshmiRepo <99910758+LakshmiRepo@users.noreply.github.com>
2022-03-18 17:24:36 -0400

    Update main.tf

--- ModuleDataSourceVM/main.tf
+++ ModuleDataSourceVM/main.tf
@@ -34,44 +34,4 @@ resource "azurerm_windows_virtual_machine" "VM" {
   }
 }
 
-/*var section*/
-
-variable "rgname" {
-  type = string
-  default = "The name of the existing resource group"
-}
-variable "size" {
-  type = string
-  default = "The size of the VM"
-}
-variable "VMName" {
-  type = string
-  default = "The name of the VM"
-}
-variable "localadminname" {
-  type = string
-  default = "The local admin account cannot be administrator"
-}
-variable "adminpwd" {
-  type = string
-  sensitive = true
-  default = "The local admin password"
-}
-variable "subnetid" {
-  type = string
-  default = "The subnet id that the network card attaches to"
-}
-variable "location" {
-  type = string
-}
-
-/* output section*/
-
-output "VM_id_out" {
-  value = azurerm_windows_virtual_machine.VM.id
-}
-
-output "VM_ip_out" {
-  value = azurerm_network_interface.VMNetworkInterface.private_ip_addresses
-}
 
