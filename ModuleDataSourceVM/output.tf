5b249d0b26889673712057cd64d5eefb3f6f081c
LakshmiRepo <99910758+LakshmiRepo@users.noreply.github.com>
2022-03-18 17:25:51 -0400

    Create output.tf

---
+++ ModuleDataSourceVM/output.tf
@@ -0,0 +1,9 @@
+/* output section*/
+
+output "VM_id_out" {
+  value = azurerm_windows_virtual_machine.VM.id
+}
+
+output "VM_ip_out" {
+  value = azurerm_network_interface.VMNetworkInterface.private_ip_addresses
+}
